913a891d7521178beca44fb215906d9ed8e97794
Sanjeevi <sanjeeviponnusamy051@gmail.com>
2026-09-14 11:22:56 +0530

    New Changes

--- utils/logger.py
+++ utils/logger.py
@@ -13,24 +13,24 @@ def get_logger(name):
 
     if not logger.handlers:
 
-        logger.setLevel(
-            logging.INFO
-        )
+        logger.setLevel(logging.INFO)
 
         formatter = logging.Formatter(
-            "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+            "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
         )
 
         file_handler = logging.FileHandler(
             "logs/automation.log"
         )
 
-        file_handler.setFormatter(
-            formatter
-        )
+        console_handler = logging.StreamHandler()
 
-        logger.addHandler(
-            file_handler
-        )
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+        logger.propagate = False
 
     return logger
